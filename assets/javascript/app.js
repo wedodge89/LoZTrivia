c99986ff75a86268eb6e8a40f2dc5e04247af4b4
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2019-10-17 18:00:34 -0400

    populated quiz questions, renamed image files for clarity

---
+++ assets/javascript/app.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+var quiz = [
+    question1 = {
+        question: "How many Legend of Zelda characters are in Super Smash Bros. Ultimate?",
+        wrong1: 9,
+        wrong2: 3,
+        wrong3: 4,
+        correct: 6},
+
+    question2 = {
+        question: "What is the name of Link's faithful steed?",
+        wrong1: "Lorse",
+        wrong2: "Shadowfax",
+        wrong3: "Zelda",
+        correct: "Epona"},
+
+    question3 = {
+        question: "HWhat is the only game in the series where 5 pieces of heart make up a heart container, rather than 4?",
+        wrong1: "Link's Awakening",
+        wrong2: "Majora's Mask",
+        wrong3: "A Link Between Worlds",
+        correct: "Twilight Princess"},
+
+    question4 = {
+        question: "What is the name of Link's recurring enemy, who looks like a monstrous pig?",
+        wrong1: "Gerudo",
+        wrong2: "Ahriman",
+        wrong3: "Vaati",
+        correct: "Ganon"},
+
+    question5 = {
+        question: "What is the name of Princess Zelda's alter ego in The Wind Waker?",
+        wrong1: "Zolda",
+        wrong2: "Linkle",
+        wrong3: "Aryll",
+        correct: "Tetra"},
+
+    question6 = {
+        question: "What is the name of the final mask in Majora's Mask, that Link can only use during boss fights?",
+        wrong1: "The Wrathful God",
+        wrong2: "The Vengeful Spirit",
+        wrong3: "The Mask of Courage",
+        correct: "The Feirce Deity"},
+
+    question7 = {
+        question: "Which of tollowing games does Link NOT appear in?",
+        wrong1: "Mario Kart 8",
+        wrong2: "Soulcalibur II",
+        wrong3: "WarioWare Gold",
+        correct: "Star Fox 64"},
+
+    question8 = {
+        question: "Which of the following is NOT a song in Ocarina of Time?",
+        wrong1: "Zelda's Lullaby",
+        wrong2: "Bolero of Fire",
+        wrong3: "Song of Storms",
+        correct: "Tempo of Time"},
+
+    question9 = {
+        question: "What are the names of the two games released for the Game Boy Color that, when beat, could carry over to a third game/true ending?",
+        wrong1: "Link's Awakening & Link's Asleepening",
+        wrong2: "Spirit Tracks & The Phantom Hourglass",
+        wrong3: "Red Version & Blue Version",
+        correct: "Oracle of Ages & Oracle of Seasons"},
+
+    question10 = {
+        question: "Which of these is not one of the three stones you must retrieve in the first part of Ocarina of Time?",
+        wrong1: "Kokiri Emerald",
+        wrong2: "Goron Ruby",
+        wrong3: "Zora Saphire",
+        correct: "Hylian Topaz"}
+]
+});
