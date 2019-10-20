4c8dd5c535216c60843db0be3009f139d795e0e7
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2019-10-20 09:42:31 -0400

    fixed bug that gave false negative on two questions. cleaned up visible HTML

--- assets/javascript/quizContent.js
+++ assets/javascript/quizContent.js
@@ -2,71 +2,71 @@
 var quiz = [
     {question: "How many Legend of Zelda characters are in Super Smash Bros. Ultimate?",
     answers: ["9", "3", "4", "6"],
-    correct: 3,
+    correct: "3",
     correctGif: "assets/images/01-ssb-twoLinks.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "What is the name of Link's faithful steed?",
     answers: ["Lorse", "Epona", "Shadowfax", "Zelda"],
-    correct: 1,
+    correct: "1",
     correctGif: "assets/images/02-tp-epona.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "What is the only game in the series where 5 pieces of heart make up a heart container, rather than 4?",
     answers: ["Twilight Princess", "Majora's Mask", "A Link Between Worlds", "Link's Awakening"],
-    correct: 0,
+    correct: "0",
     correctGif: "assets/images/03-la-heartPiece.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "What is the name of Link's recurring enemy, who looks like a monstrous pig?",
     answers: ["Gerudo", "Ahriman", "Ganon", "Vaati"],
-    correct: 2,
-    correctGif: "assets/images/04-alttp-ganon.gif />",
+    correct: "2",
+    correctGif: "assets/images/04-alttp-ganon.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "What is the name of Princess Zelda's alter ego in The Wind Waker?",
     answers: ["Zolda", "Aryll", "Linkle", "Tetra"],
-    correct: 3,
-    correctGif: "assets/images/05-ww-tetra.gif />",
+    correct: "3",
+    correctGif: "assets/images/05-ww-tetra.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
     
     {question: "What is the name of the final mask in Majora's Mask, that Link can only use during boss fights?",
     answers: ["The Fierce Deity", "The Wrathful God", "The Vengeful Spirit", "The Mask of Courage"],
-    correct: 0,
-    correctGif: "assets/images/06-hw-fierceDeity.gif />",
+    correct: "0",
+    correctGif: "assets/images/06-hw-fierceDeity.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "Which of tollowing games does Link NOT appear in?",
     answers: ["Mario Kart 8", "Soulcalibur II", "Star Fox 64", "WarioWare Gold"],
-    correct: 2,
-    correctGif: "assets/images/07-mk8-linkBike.gif />",
+    correct: "2",
+    correctGif: "assets/images/07-mk8-linkBike.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "Which of the following is NOT a song in Ocarina of Time?",
     answers: ["Zelda's Lullaby", "Bolero of Fire", "Song of Storms", "Tempo of Time"],
-    correct: 3,
-    correctGif: "assets/images/08-oot-ocarina.gif />",
+    correct: "3",
+    correctGif: "assets/images/08-oot-ocarina.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "What are the names of the two games released for the Game Boy Color that, when beat, could carry over to a third game/true ending?",
     answers: ["Link's Awakening & Link's Asleepening", "Spirit Tracks & The Phantom Hourglass", "Oracle of Ages & Oracle of Seasons", "Red Version & Blue Version"],
-    correct: 2,
-    correctGif: "assets/images/09-ooas-horse.gif />",
+    correct: "2",
+    correctGif: "assets/images/09-ooas-horse.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     },
 
     {question: "Which of these is NOT one of the three stones you must retrieve in the first part of Ocarina of Time?",
     answers: ["Kokiri Emerald", "Goron Ruby", "Zora Sapphire", "Hylian Topaz"],
-    correct: 3,
-    correctGif: "assets/images/10-art-threeStones.gif />",
+    correct: "3",
+    correctGif: "assets/images/10-art-threeStones.gif",
     wrongGif: "assets/images/wrong-ww-wave.gif"
     }
 ]
